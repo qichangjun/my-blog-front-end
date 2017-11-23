5ea89aa65be4ef07170685fd05b77f59d3597fe0
qichangjun <1061427304@qq.com>
2017-11-23 18:01:14 +0800

    添加广告进出场动画

--- src/app/share/advertising/advertising.component.ts
+++ src/app/share/advertising/advertising.component.ts
@@ -1,9 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
+
 @Component({
   selector: 'app-advertising',
   templateUrl: './advertising.component.html',
-  styleUrls: ['./advertising.component.css']
+  styleUrls: ['./advertising.component.css'],
+  animations: [
+    trigger('flyInOut', [
+      state('in', style({transform: 'translateX(0)'})),
+      transition('void => *', [
+        style({transform: 'translateY(100%)'}),
+        animate(1000)
+      ]),
+      transition('* => void', [
+        animate(1000, style({transform: 'translateY(100%)'}))
+      ])
+    ])
+  ]
 })
 export class AdvertisingComponent implements OnInit {
   isClosed : boolean = false;
