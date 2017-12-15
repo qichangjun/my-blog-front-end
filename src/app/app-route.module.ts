a34c0ebc9ccf2e143a69c313bfa38892f0d95865
qichangjun <1061427304@qq.com>
2017-12-15 14:14:33 +0800

    调整路由模块

--- src/app/app-route.module.ts
+++ src/app/app-route.module.ts
@@ -3,9 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AuthGuard } from './core/guards/auth.guards';
 const routes:Routes = [
-  { path:'',redirectTo:'main/list',pathMatch:'full'},
-  { path:'login',loadChildren:'app/login/login.module#LoginModule'},
-  { path:'main',loadChildren:'app/main/main.module#MainModule',canActivate:[AuthGuard]}
+  { path:'',redirectTo:'login',pathMatch:'full'},  
+  { path:'login',loadChildren:'./login/login.module#LoginModule'},
+  { path:'main',loadChildren:'./main/main.module#MainModule',canActivate:[AuthGuard]},
+  { path:'**',redirectTo:'main'}
 ];
 
 @NgModule({
